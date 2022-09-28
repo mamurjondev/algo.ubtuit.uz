a92c6d71bd221749d1bc7645f958dc8ec6b098c9
mamurjondev <93238815+mamurjondev@users.noreply.github.com>
2022-09-28 21:57:22 +0600

    Create 0002.cpp

---
+++ 0002.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+float r1, r2, r3, s1, s2, s3;
+cin >> r1 >> r2 >> r3;
+s1 = M_PI * r1 * r1;
+s2 = M_PI * r2 * r2;
+s3 = M_PI * r3 * r3;
+cout << setprecision(2) << fixed << s1 << ' ' << s2 << ' ' << s3 << '\n';
+}    
