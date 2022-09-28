48b9f93a3d13044f507e3f89f2cf514ffddedacc
mamurjondev <93238815+mamurjondev@users.noreply.github.com>
2022-09-28 22:02:16 +0600

    Create 0008.cpp

---
+++ 0008.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+   float v,s;
+   float t;   
+   cin >>v>>s;    
+   t = s/v;
+   printf("%.2f",t);
+   
+   
+   return 0;
+}
