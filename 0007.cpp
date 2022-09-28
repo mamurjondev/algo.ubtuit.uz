17b95911269bd1ba11d412c7fffa8790f9aa25d3
mamurjondev <93238815+mamurjondev@users.noreply.github.com>
2022-09-28 22:00:41 +0600

    Create 0007.cpp

---
+++ 0007.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+     
+int main(){
+        float r,h;
+        cin>>h>>r;
+        printf ("%.2f ", M_PI*r*r*h/3);
+        return 0;
+}
