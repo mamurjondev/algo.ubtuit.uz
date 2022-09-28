553a2e72eaca930e5d2cd9b10599ce82a7615695
mamurjondev <93238815+mamurjondev@users.noreply.github.com>
2022-09-28 21:59:10 +0600

    Create 0005.cpp

---
+++ 0005.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define _sf(x) setprecision(x) << fixed
+typedef long int li;
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	float yp;
+	yp = (a + b + c) / 2.;
+	cout << setprecision(2) << fixed << yp << '\n';
+}
