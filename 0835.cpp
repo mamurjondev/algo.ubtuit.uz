a69ea1151d5d7ec30533b5f2dd7d315523aca2a4
mamurjondev <93238815+mamurjondev@users.noreply.github.com>
2022-09-28 22:23:45 +0600

    Create 0835.cpp

---
+++ 0835.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string n;
+	cin>>n;
+	if (n=="March"){
+		cout<<"spring";
+	} else if (n=="August"){
+		cout<<"summer";
+	} else if (n=="December"){
+		cout<<"winter";
+	} else if (n=="November"){
+		cout<<"autumn";
+	} else if (n=="April"){
+		cout<<"spring";
+	} else if (n=="May"){
+		cout<<"spring";
+	} else if (n=="June"){
+		cout<<"summer";
+	} else if (n=="July"){
+		cout<<"summer";
+	} else if (n=="January"){
+		cout<<"winter";
+	} else if (n=="February"){
+		cout<<"winter";
+	} else if (n=="October"){
+		cout<<"autumn";
+	} else if (n=="September"){
+		cout<<"autumn";
+	} 
+
+	return 0;
+}      
