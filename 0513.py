183ee462ba29596c7ce099ce5f8f27b281143e6e
mamurjondev <93238815+mamurjondev@users.noreply.github.com>
2022-09-28 22:46:19 +0600

    Create 0513.py

---
+++ 0513.py
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+double L, T;
+cin>>L;
+float const gi=9.81;
+T=2*M_PI*sqrt(2.0*L/(3*gi));
+cout<<setprecision(4)<<fixed<<T;
+
+   return 0;
+}    
