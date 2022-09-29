a4710679459da716fde91293c1955dade6173f79
mamurjondev <93238815+mamurjondev@users.noreply.github.com>
2022-09-29 10:06:02 +0500

    Create 0509.cpp

---
+++ 0509.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int m, k1, k2;
+cin>>m>>k1>>k2;
+double T;
+
+T=2*M_PI*sqrt(1.0*m/(k1+k2));
+cout<<setprecision(4)<<fixed<<T;
+
+   return 0;
+}    
