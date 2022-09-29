d79abbd6b51820aa5886b288995843e2dc4eaed1
mamurjondev <93238815+mamurjondev@users.noreply.github.com>
2022-09-29 10:20:07 +0500

    Create 0334.cpp

---
+++ 0334.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ #include <bits/stdc++.h>
+ using namespace std;
+ int main(){
+     double v1,v2,v3,s;
+     cin>>v1>>v2>>v3;
+     s=(v1+v2+v3)/3;
+     cout.precision(2);
+     cout<<fixed<<s;
+ return 0;
+ }    
