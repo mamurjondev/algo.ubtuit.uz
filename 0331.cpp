b9014ef98f0a1d80d99aa93952720872aa33af10
mamurjondev <93238815+mamurjondev@users.noreply.github.com>
2022-09-29 10:21:25 +0500

    Create 0331.cpp

---
+++ 0331.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ #include <cmath>
+ #include <iomanip>
+
+ using namespace std;
+
+ int main()
+ {
+     int a,r;
+     cin>>a>>r;
+     if(a>=2*r) cout<<"Yes";
+     else cout<<"No";
+     return 0;
+ }    
